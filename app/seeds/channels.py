cd1eceeaa09606f51cf0ae1caa5ee55cea422039
nachen98 <nachen98@gmail.com>
2022-10-17 21:21:58 -0700

    added seeders data for channels, servers and messages tables

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -12,8 +12,8 @@ def seed_channels():
     )
 
     server_1_channel_2 = Channel(
-        name='welcome',
-        topic='greetings',
+        name='music',
+        topic='classic, pop, r&b',
         is_voice=False,
         server_id=1,
         created_at=datetime.now(),
@@ -21,8 +21,44 @@ def seed_channels():
     )
 
     server_1_channel_3 = Channel(
-        name='welcome',
-        topic='greetings',
+        name='food',
+        topic='American, Mexican, Chinese',
+        is_voice=False,
+        server_id=1,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+    server_1_channel_4 = Channel(
+        name='coding',
+        topic='python, C++, Javascript, Java',
+        is_voice=False,
+        server_id=1,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+    server_1_channel_5 = Channel(
+        name='pets',
+        topic='cats, dogs, fish, bunnies',
+        is_voice=False,
+        server_id=1,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+    server_1_channel_6 = Channel(
+        name='sports',
+        topic='fencing, baseball, basketball, soccer',
+        is_voice=False,
+        server_id=1,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+    server_1_channel_7 = Channel(
+        name='books',
+        topic='fables, classics, fiction, adventure stories',
         is_voice=False,
         server_id=1,
         created_at=datetime.now(),
@@ -42,7 +78,11 @@ def seed_channels():
     db.session.add(server_1_channel_2)
     db.session.add(server_1_channel_3)
     db.session.add(server_2_channel_1)
-
+    db.session.add(server_1_channel_4)
+    db.session.add(server_1_channel_5)
+    db.session.add(server_1_channel_6)
+    db.session.add(server_1_channel_7)
+    
     db.session.commit()
 
 def undo_channels():
