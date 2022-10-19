8a7e50f9b2bfa99f38537608b8f6f63745ff92cc
Justin <hangjustin@gmail.com>
2022-10-18 18:23:51 -0700

    Dynamically render channel and users

--- react-app/src/components/ChannelOrDmSidebar/index.js
+++ react-app/src/components/ChannelOrDmSidebar/index.js
@@ -10,6 +10,7 @@ const ChannelOrDmSidebar = () => {
     const { channelId, serverId } = useParams()
     const allRegularServers = useSelector(state => state.regularServerReducer)
     const allDmServers = useSelector(state => state.dmServerReducer)
+    const allChannels = useSelector(state => state.channelReducer)
     const [serverName, setServername] = useState('');
     const pathLocation = useLocation();
     const serverIdInt = parseInt(serverId)
@@ -23,19 +24,28 @@ const ChannelOrDmSidebar = () => {
         setServername(server.name)
     }, [serverIdInt])
 
+    // IF pathLocation.pathname === '/channels/@me' change the way that it is rendered
+    // Will have to pass in dmServers rather than channels
+
+
     if (Object.values(allRegularServers).length === 0) return <span>Loading...</span>
 
     const server = allRegularServers[serverIdInt] ?
-                    allRegularServers[serverIdInt] : allDmServers[serverIdInt];
+                    allRegularServers[serverIdInt] : allDmServers[serverIdInt] ?
+                    allDmServers[serverIdInt] : false
+
+    // server === false indicates that it is likely at /channels/@me
+    //      ... or it's an edge case.
+    let channels;
+    if (server === false) {
+        // If it is at /channels/@me, then will have to grab dm_servers
+        channels = Object.values(allDmServers)
+    }
 
     const serverImg = 'https://staticc.sportskeeda.com/editor/2022/03/199c9-16468646951546-1920.jpg';
     const is_dm = false;
 
-    const channels = [
-        'general',
-        'welcome',
-        'coding'
-    ]
+    channels = server.channels.map(channelId => allChannels[channelId])
 
     // TEMPORARY - TO BE ABLE TO LOAD '/channels/@me'
 
@@ -53,11 +63,11 @@ const ChannelOrDmSidebar = () => {
         <div id='channel-or-dm-sidebar' className='flx-col'>
             {!is_dm && <ServerBanner serverName={serverName} serverImg={serverImg}/>}
 
-            {channels.map((channel, ind) =>{
+            {channels.map((channel) =>{
                 return (
-                    <NavLink to='/channels/@me' key={ind}>
+                    <NavLink to={`/channels/${serverId}/${channel.id}`} key={channel.id}>
                         <div className='server-channel-card flx-row-align-ctr'>
-                            # {channel}
+                            # {channel.name}
                         </div>
                     </NavLink>
                 )
--- react-app/src/components/UsersListSidebar/index.js
+++ react-app/src/components/UsersListSidebar/index.js
@@ -1,20 +1,29 @@
 
+import { useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom'
 import './UsersListSidebar.css'
 
 const UsersListSidebar = () => {
-    const users = [
-        'Na',
-        'Yibo',
-        'Justin',
-        'David'
-    ]
+    const { serverId } = useParams();
+    const allUsers = useSelector(state => state.usersReducer);
+    const allRegularServers = useSelector(state => state.regularServerReducer);
+    const server = allRegularServers[serverId];
+
+    // TEMPORARY... WILL HAVE TO CONDITIONALL RENDER IN THE ChatBox COMPONENT
+    // NOT DOING NOW SINCE YIBO IS WORKING ON THAT COMPONENT
+    // WILL HAVE TO RESIZE CHATBOX COMPONENT ACCORDINGLY
+    const pathLocation = useLocation();
+    if (pathLocation.pathname === '/channels/@me') return (<span>Nothing to see here. Move along</span>)
+
+    if (!server) return (<span>Loading...</span>)
+    const serverUserIds = server.users
 
     return (
         <div id='users-list-sidebar' className='flx-col-algn-ctr'>
-            {users.map((user, ind) => {
+            {serverUserIds.map((userId) => {
                 return (
-                    <div key={ind} className='user-card flx-row-align-ctr'>
-                        {user}
+                    <div key={userId} className='user-card flx-row-align-ctr'>
+                        {allUsers[userId].username}
                     </div>
                 )
             })}
