785f24a6e1eb187cb18b49f2c84c91572b7c4ac6
nachen98 <nachen98@gmail.com>
2022-10-19 09:57:59 -0700

    styled for login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import { Link } from "react-router-dom"
 import './LoginForm.css'
 
 const LoginForm = () => {
@@ -15,6 +16,7 @@ const LoginForm = () => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
+      console.log("data!!!!!!!!!", data)
       setErrors(data);
     }
   };
@@ -27,63 +29,66 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
-  const demoUserLogin=(e)=>{
+  const demoUserLogin = (e) => {
     e.preventDefault();
-    const demoUser = { email: 'demo@aa.io', password:'password'}
-    dispatch(login(demoUser))
+    dispatch(login('demo@aa.io', 'password'))
   }
   if (user) {
     return <Redirect to='/' />;
   }
 
   return (
-  
+
     <div id="login-container">
 
-    
-    <div id="login-box">
-      <div id="inner-container">
 
-      
-      <div id="welcome" className='flx-col-justify-algn-ctr'>Welcome back!</div>
-      <div id="under-welcome" className='flx-col-justify-algn-ctr'>We're so excited to see you again!</div>
-      <form onSubmit={onLogin} className='flx-col'>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+      <div id="login-box">
+        <div id="inner-container">
+
+
+          <div id="welcome" className='flx-col-justify-algn-ctr'>Welcome back!</div>
+          <div id="under-welcome" className='flx-col-justify-algn-ctr'>We're so excited to see you again!</div>
+          <form onSubmit={onLogin} className='flx-col'>
+            <div id="error-messages">
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
+            </div>
+            <div >
+              <label htmlFor='email'>EMAIL
+                <span id="required-field">*</span>
+              </label>
+              <input
+                name='email'
+                type='text'
+                //placeholder='Email'
+                value={email}
+                onChange={updateEmail}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor='password'>PASSWORD
+                <span id="required-field">*</span>
+              </label>
+              <input
+                name='password'
+                type='password'
+                //placeholder='Password'
+                value={password}
+                onChange={updatePassword}
+                required
+              />
+              <button type='submit' id="login-button">Log In</button>
+              <button type='submit' id="demo-user" onClick={demoUserLogin}>Log In As Demo User</button>
+              <div id="signup-meessage">
+                <div id="need-account">Need an account?</div>
+                <Link to={"/sign-up"} id="linktosignup">Register</Link>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      <div >
-        <label htmlFor='email'>EMAIL
-        <span id="required-field">*</span>
-        </label>
-        <input
-          name='email'
-          type='text'
-          //placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>PASSWORD
-        <span id="required-field">*</span>
-        </label>
-        <input
-          name='password'
-          type='password'
-          //placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-          required
-        />
-        <button type='submit' id="login-button">Log In</button>
-        <button type='submit' id="demo-user" onClick={demoUserLogin}>Log In As Demo User</button>
-      </div>
-    </form>
-    </div>
-    </div>
     </div>
   );
 };
