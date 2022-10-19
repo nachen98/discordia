9c63b9ff8a95f39c8cd3dbfa4eeb4c1779f727f7
nachen98 <nachen98@gmail.com>
2022-10-18 23:18:10 -0700

    started with styling and implementing login form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -26,39 +27,64 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
+  const demoUserLogin=(e)=>{
+    e.preventDefault();
+    const demoUser = { email: 'demo@aa.io', password:'password'}
+    dispatch(login(demoUser))
+  }
   if (user) {
     return <Redirect to='/' />;
   }
 
   return (
-    <form onSubmit={onLogin}>
+  
+    <div id="login-container">
+
+    
+    <div id="login-box">
+      <div id="inner-container">
+
+      
+      <div id="welcome" className='flx-col-justify-algn-ctr'>Welcome back!</div>
+      <div id="under-welcome" className='flx-col-justify-algn-ctr'>We're so excited to see you again!</div>
+      <form onSubmit={onLogin} className='flx-col'>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
+      <div >
+        <label htmlFor='email'>EMAIL
+        <span id="required-field">*</span>
+        </label>
         <input
           name='email'
           type='text'
-          placeholder='Email'
+          //placeholder='Email'
           value={email}
           onChange={updateEmail}
+          required
         />
       </div>
       <div>
-        <label htmlFor='password'>Password</label>
+        <label htmlFor='password'>PASSWORD
+        <span id="required-field">*</span>
+        </label>
         <input
           name='password'
           type='password'
-          placeholder='Password'
+          //placeholder='Password'
           value={password}
           onChange={updatePassword}
+          required
         />
-        <button type='submit'>Login</button>
+        <button type='submit' id="login-button">Log In</button>
+        <button type='submit' id="demo-user" onClick={demoUserLogin}>Log In As Demo User</button>
       </div>
     </form>
+    </div>
+    </div>
+    </div>
   );
 };
 
