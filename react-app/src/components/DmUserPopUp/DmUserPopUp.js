18488fe45c1e4abaf007f15f9c6f262e9b7d4a06
Justin <hangjustin@gmail.com>
2022-10-20 19:09:03 -0700

    Added dm functionality from server-users list

---
+++ react-app/src/components/DmUserPopUp/DmUserPopUp.js
@@ -0,0 +1,111 @@
+
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom';
+import { create_dm } from '../../store/messages';
+
+import './DmUserPopUp.css'
+
+const COLORS = ['gray', 'purple', 'red', 'yellow', 'green'];
+
+const DmUserPopUp = ({ socket, userId }) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const sessionUser = useSelector(state => state.session.user)
+    const allUsers = useSelector(state => state.usersReducer)
+    const allDmServersArr = Object.values(useSelector(state => state.dmServerReducer))
+
+    const [dmMessage, setDmMessage] = useState('');
+
+    const selectedUser = allUsers[+userId]
+    const colorInd = COLORS[userId % COLORS.length];
+
+    const dmServer = allDmServersArr.find((server) => {
+        if(
+            server.name === `${sessionUser.id}-${userId}`
+            ||
+            server.name === `${userId}-${sessionUser.id}` ){
+                return true;
+            }
+    })
+
+    useEffect(() => {
+        const dmPopup = document.getElementById('dm-user-pop-up');
+
+        const stopClose = e => e.stopPropagation();
+
+        dmPopup.addEventListener('click', stopClose)
+
+        return () => dmPopup.removeEventListener('click', stopClose)
+    }, [])
+
+    const handleSendDm = e => {
+        e.preventDefault();
+        if (!dmMessage.length) return;
+        if (dmMessage.length > 255) return alert(`255 characters max. Your message was ${dmMessage.length} characters long.`)
+
+        if (dmServer) {
+            socket.send('message',
+            {
+                "sender_id": sessionUser.id,
+                "is_channel_message": false,
+                "dm_server_id": dmServer.id,
+                "body": dmMessage
+            })
+            console.log('made it here?')
+            socket.on('hello', (data) => {
+                console.log('data received back from socket is ', data)
+                dispatch(create_dm(data))
+                history.push(`/channels/${dmServer.id}`)
+                return;
+            })
+        }
+
+        if (!dmServer) {
+            alert('Wait up... Need to create a dm server between you two')
+        }
+
+    }
+
+
+    return (
+        <div id='dm-user-pop-up' className='dm-user-pop-up-container pos-abs flx-col'>
+            <div className={`user-popup-banner ${colorInd}-bg`} />
+
+            <div className='profile-pic-bg pos-abs flx-row-justify-align-ctr'>
+                <div className={`${colorInd}-bg pop-up-profile-img-container flx-row-justify-align-ctr`}>
+                    <img className='pop-up-profile-img' src='https://pnggrid.com/wp-content/uploads/2021/05/Discord-Logo-White-1024x780.png'/>
+                </div>
+            </div>
+
+            <div id='dm-pop-up__user-profile' className='flx-col'>
+                <div id='dm-pop-up__username'>
+                    {selectedUser.username}
+                </div>
+
+                {selectedUser.id === sessionUser.id &&
+                    <img id='spiderman' alt='talking-to-yourself' src='https://i.imgur.com/X1JLM7l.jpg' />
+                }
+
+                {selectedUser.id !== sessionUser.id && (
+                <>
+                    <div id='send-user-a-msg'>Send {selectedUser.username} a direct message!</div>
+
+                    <form id='send-dm' className='flx-row-justify-align-ctr' onSubmit={handleSendDm}>
+                        <input
+                            className='send-dm-input'
+                            placeholder={`Message @${selectedUser.username}`}
+                            onChange={(e) => setDmMessage(e.target.value)}
+                        />
+                    </form>
+                </>)}
+            </div>
+
+        </div>
+    )
+
+
+}
+
+export default DmUserPopUp
--- react-app/src/components/UsersListSidebar/index.js
+++ react-app/src/components/UsersListSidebar/index.js
@@ -1,15 +1,34 @@
 
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom'
+import DmUserPopUp from '../DmUserPopUp/DmUserPopUp';
+import UserCard from './UserCard';
 import './UsersListSidebar.css'
 
 const COLORS = ['gray', 'purple', 'red', 'yellow', 'green'];
 
-const UsersListSidebar = () => {
+const UsersListSidebar = ({ socket }) => {
     const { serverId } = useParams();
     const allUsers = useSelector(state => state.usersReducer);
     const allRegularServers = useSelector(state => state.regularServerReducer);
     const server = allRegularServers[serverId];
+
+    const [showDmPopUp, setShowDmPopUp] = useState(false);
+
+    const openPopUp = () => {
+        if (showDmPopUp) return;
+        setShowDmPopUp(true);
+    }
+
+    useEffect(() => {
+        if (!showDmPopUp) return;
+
+        const closeDmPopUp = () => setShowDmPopUp(false);
+        document.addEventListener('click', closeDmPopUp);
+
+        return () => document.removeEventListener('click', closeDmPopUp)
+    }, [showDmPopUp])
 
     // TEMPORARY... WILL HAVE TO CONDITIONALL RENDER IN THE ChatBox COMPONENT
     // NOT DOING NOW SINCE YIBO IS WORKING ON THAT COMPONENT
@@ -22,19 +41,7 @@ const UsersListSidebar = () => {
 
     return (
         <div id='users-list-sidebar' className='flx-col-align-ctr'>
-            {serverUserIds.map((userId, ind) => {
-                const colorInd = userId % COLORS.length;
-                return (
-                    <div key={userId} className='user-card flx-row-align-ctr'>
-
-                        <div className={`dm-logo-container flx-row-justify-align-ctr ${COLORS[colorInd]}-bg`}>
-                            <img className='dm-logos' src='https://pnggrid.com/wp-content/uploads/2021/05/Discord-Logo-White-1024x780.png' />
-                        </div>
-
-                        {allUsers[userId].username}
-                    </div>
-                )
-            })}
+            {serverUserIds.map((userId) => <UserCard key={userId} socket={socket} userId={userId} />)}
         </div>
     )
 }
