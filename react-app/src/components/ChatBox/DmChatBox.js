d5dd3b794f3e2afe7a61cf3f793e61be7287e229
Justin <hangjustin@gmail.com>
2022-10-23 01:51:38 -0700

    Prevent user from sending message longer than 255 chars and adjusted seed ata

--- react-app/src/components/ChatBox/DmChatBox.js
+++ react-app/src/components/ChatBox/DmChatBox.js
@@ -20,8 +20,18 @@ const DmChatBox = ({socket, dmMessages}) =>{
     const allDmServers = useSelector(state => state.dmServerReducer)
     const current_user = useSelector(state => state.session.user)
     const [messageInput, setMessageInput] = useState('')
+    const [messageInputLengthErr, setMessageInputLengthErr] = useState('gray-text')
     const COLORS = ['gray', 'purple', 'red', 'yellow', 'green'];
-    
+
+    useEffect(() => {
+        if (messageInput.length <= 255 && messageInputLengthErr=== 'gray-text') return;
+        if (messageInput.length > 255 && messageInputLengthErr === 'red-text') return;
+
+        if (messageInput.length > 255 && messageInputLengthErr === 'gray-text') setMessageInputLengthErr('red-text')
+        if (messageInput.length <= 255 && messageInputLengthErr ==='red-text') setMessageInputLengthErr('gray-text');
+
+    }, [messageInput])
+
     const handleMessageInput = (e) => {
         setMessageInput(e.target.value)
     }
@@ -32,14 +42,15 @@ const DmChatBox = ({socket, dmMessages}) =>{
     }
 
     const handleMessageSubmit = async (e) => {
-       e.preventDefault();
-       console.log("status of socket", socket)
-       socket.send('message', 
-        {   
-            "sender_id": current_user.id, 
-            "is_channel_message": false, 
-            "dm_server_id": serverId, 
-            "body": messageInput 
+        e.preventDefault();
+        if (messageInput.length > 255) return;
+        console.log("status of socket", socket)
+        socket.send('message',
+        {
+            "sender_id": current_user.id,
+            "is_channel_message": false,
+            "dm_server_id": serverId,
+            "body": messageInput
         })
         setMessageInput('');
         return;
@@ -61,9 +72,9 @@ const DmChatBox = ({socket, dmMessages}) =>{
     console.log("dataObj--users1" ,users);
     const dmServer = allDmServers[serverId];
     const userIds = dmServer.name.split("-");
-    
+
     const otherUser = userIds.filter(id => parseInt(id) != current_user.id)
-    
+
     const otherUserName = users[parseInt(otherUser[0])].username
     console.log("other user name ---", otherUserName)
 
@@ -73,24 +84,24 @@ const DmChatBox = ({socket, dmMessages}) =>{
             <div key = {index} className='all-messages-container'>
                     <div className='date-divider'> {getMonthYear(key)}</div>
                     {  dateObj[key].sort((a,b) =>a.id-b.id).map((item, idx) =>{
-                        const colorInd = item.user_id % COLORS.length; 
+                        const colorInd = item.user_id % COLORS.length;
                         return (
                             <div key = {idx} className="channel-message-container">
                                 <div className={`channel-message-user-profile-image-container ${COLORS[colorInd]}-bg flx-row-justify-align-ctr`}>
                                     <img className={`channel-message-user-profile-image`} src={"https://pnggrid.com/wp-content/uploads/2021/05/Discord-Logo-White-1024x780.png"} alt={"bb"}/>
                                 </div>
-                                <div className="channel-message-detail"> 
+                                <div className="channel-message-detail">
                                     <div className='channel-message-info'>
-                                      <div className='channel-message-user-fullname'>  {users[item.user_id].username}  </div>
-                                      <div className="channel-message-created-date"> {new Date(item.created_at).toLocaleDateString()} </div>
+                                        <div className='channel-message-user-fullname'>  {users[item.user_id].username}  </div>
+                                        <div className="channel-message-created-date"> {new Date(item.created_at).toLocaleDateString()} </div>
                                     </div>
                                     <div className="channel-message-body" > {item.body}</div>
                                 </div>
-                            </div>  
+                            </div>
                             )
                         })
-                    }       
-            </div>   
+                    }
+            </div>
         )
     })
 
@@ -117,7 +128,7 @@ const DmChatBox = ({socket, dmMessages}) =>{
                         />
                     <button type='submit' id='send-message-btn' style={{display: 'none'}} />
                     </form>
-                    <span className='message-char-count'>{255 - messageInput.length}</span>
+                    <span className={`${messageInputLengthErr} message-char-count`}>{255 - messageInput.length}</span>
                 </div>
             </div>
 
